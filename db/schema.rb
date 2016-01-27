18e69c7792700db91bbf69f38480c1a5971bd722
Ronald Chacon <ronald.chacon87@gmail.com>
2016-01-27 04:05:44 -0800

    Add is_completed field to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127080412) do
+ActiveRecord::Schema.define(version: 20160127120512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.date     "due_date"
+    t.text     "notes"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.boolean  "is_completed"
+  end
+
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,4 +48,5 @@ ActiveRecord::Schema.define(version: 20160127080412) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "tasks", "users"
 end
