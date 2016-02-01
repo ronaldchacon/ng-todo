f919aa15a4e19a0ffd136c9d80f477e9aa03e0c1
Ronald Chacon <ronald.chacon87@gmail.com>
2016-02-01 00:18:09 -0800

    Updated tasks/index.html to show todo/completed. tasks#index action only shows current_user todos

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tasks = Task.all
+    @tasks = Task.all.where({ user: current_user })
     respond_with @tasks
   end
 
